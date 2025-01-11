55d43ce1df7f5c4ea4dc75df8b4a35a920f93226
Daniyar Kabdulin <mrdocula@gmail.com>
2025-01-11 10:47:31 +0200

    9. Palindrome Number

---
+++ src/main/java/com/dan/leetcode/problem0009/Solution2.java
@@ -0,0 +1,19 @@
+package com.dan.leetcode.problem0009;
+
+class Solution2 {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+
+        int i = 0;
+        int j = s.length() - 1;
+
+        while(i <= j)
+        {
+            if(s.charAt(i) != s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
