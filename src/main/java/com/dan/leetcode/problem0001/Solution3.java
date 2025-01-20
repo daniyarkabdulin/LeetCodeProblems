e1f72fba8695d824b974c1cec6aa83cfd4dd2009
Daniyar Kabdulin <mrdocula@gmail.com>
2025-01-20 23:15:28 +0200

    1. Two Sum. Add new class Solution3

---
+++ src/main/java/com/dan/leetcode/problem0001/Solution3.java
@@ -0,0 +1,13 @@
+package com.dan.leetcode.problem0001;
+
+class Solution3 {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i=1;i<nums.length;i++)
+            for(int j=i;j<nums.length;j++)
+                if(nums[j]+nums[j-i]==target)
+                {
+                    return new int[]{j,j-i};
+                }
+        return null;
+    }
+}
